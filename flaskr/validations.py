166b48643456c085f13c7972be90e37aa70df039
Rowe10 <158099228+Rowe10@users.noreply.github.com>
2024-01-30 11:46:20 +0000

    moved from models

--- flaskr/validations.py
+++ flaskr/validations.py
@@ -1,1 +1,26 @@
+#Firstly Define All Validations
+
+
+def LengthCheck(String, Max, Min):
+  
+  value = len(String)
+
+  if value < Max and value > Min:
+    return True
+    
+  else:
+    return False
+
+        #Hours Format Check
+
+def HoursCheck(String):
+
+  value = len(String)
+
+  if value != 8 and value != 7:
+    return False
+
+        #Isolate Numbers In The String
+
+#Seconds = Mid(String, (value - 1), value)
 
